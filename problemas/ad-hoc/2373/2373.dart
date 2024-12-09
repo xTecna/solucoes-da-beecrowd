521641986b7f62391343c16ab66966879e52b333
xTecna <raquelmarcolino25@gmail.com>
2024-12-09 12:52:40 -0300

    Mais pequenos ajustes.

--- problemas/ad-hoc/2373/2373.dart
+++ problemas/ad-hoc/2373/2373.dart
@@ -1,9 +1,18 @@
 import 'dart:io';
-
-void main()
-{
-	int a = int.parse(stdin.readLineSync()!);
-	int b = int.parse(stdin.readLineSync()!);
-
-	print("X = ${a + b}");
+ 
+void main() {
+    int n = int.parse(stdin.readLineSync()!);
+    
+    int resposta = 0;
+    for (int i = 0; i < n; ++i) {
+        List<int> entrada = stdin.readLineSync()!.split(' ').map((x) => int.parse(x)).toList();
+        int latas = entrada[0];
+        int copos = entrada[1];
+        
+        if (latas > copos) {
+            resposta += copos;
+        }
+    }
+    
+    print(resposta);
 }
